09a6d857b57f45220ea44097ca173899d1cb9662
Slawomir Jaranowski <s.jaranowski@gmail.com>
2022-03-03 19:41:41 +0100

    Site build improvement
    - build site only in ff
    - upload site as GH artifact
    - parent pom 35
    - example java code

---
+++ src/main/java/org/apache/test/TestApp.java
@@ -0,0 +1,51 @@
+package org.apache.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Test app
+ */
+public class TestApp
+{
+
+    public static void main( String[] args )
+    {
+        m1();
+        m2();
+    }
+
+    /**
+     * FIXME in javadoc
+     */
+    private static void m1()
+    {
+        System.out.println( "Hello" );
+    }
+
+    private static void m2()
+    {
+        System.out.println( "Hello" );
+    }
+
+    private static void m3()
+    {
+        // TODO test tag
+    }
+}
