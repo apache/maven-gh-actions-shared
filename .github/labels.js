a88d2184e6c141d5006beb145ed404baa793cfdf
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-01-01 18:30:54 +0100

    Labels synchronization action

---
+++ .github/labels.js
@@ -0,0 +1,74 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for th
+// specific language governing permissions and limitations
+// under the License.
+
+module.exports = [
+    {
+        name: "bug",
+        color: "d73a4a",
+        description: "Something isn't working"
+    },
+    {
+        name: "dependencies",
+        color: "0366d6",
+        description: "Pull requests that update a dependency file"
+    },
+    {
+        name: "java",
+        color: "ffa221",
+        description: "Pull requests that update Java code"
+    },
+    {
+        name: "documentation",
+        color: "0075ca",
+        description: "Improvements or additions to documentation"
+    },
+    {
+        name: "duplicate",
+        color: "cfd3d7",
+        description: "This issue or pull request already exists"
+    },
+    {
+        name: "enhancement",
+        color: "a2eeef",
+        description: "New feature or request"
+    },
+    {
+        name: "good first issue",
+        color: "7057ff",
+        description: "Good for newcomers"
+    },
+    {
+        name: "help wanted",
+        color: "008672",
+        description: "Extra attention is needed"
+    },
+    {
+        name: "invalid",
+        color: "e4e669",
+        description: "This doesn't seem right"
+    },
+    {
+        name: "maintenance",
+        color: "912E29",
+        description: ""
+    },
+    {
+        name: "wontfix",
+        color: "ffffff",
+        description: "This will not be worked on"
+    }
+]
